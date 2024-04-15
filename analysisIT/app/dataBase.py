40858c2024c5e8fca47e18b43bea4f8fe608d14f
IlyaZawyalow <ilyazawyalow@gmail.com>
2024-04-15 18:11:47 +0300

    made an abstract

--- analysisIT/app/dataBase.py
+++ analysisIT/app/dataBase.py
@@ -2,9 +2,12 @@ import psycopg2
 from psycopg2 import IntegrityError
 import json
 from loguru import logger
-
+from typing import Dict, Any
 
 class DataBase:
+    """
+    Сlass with functions for working with a database
+    """
 
     def __init__(self, dbname: str, username: str, password: str, host: str):
         self.dbname = dbname
@@ -14,41 +17,43 @@ class DataBase:
         self.conn = None
         self.cur = None
 
-    def connect(self, tableName: str):
+    def connect(self, table_name: str) -> None:
         try:
-            self.conn = psycopg2.connect(database=self.dbname, user=self.username, host=self.host, password=self.password)
+            self.conn = psycopg2.connect(database=self.dbname, user=self.username, host=self.host,
+                                         password=self.password)
             self.cur = self.conn.cursor()
 
-            create_table_query = f'CREATE TABLE IF NOT EXISTS {tableName} (vacancies_id SERIAL PRIMARY KEY, data_jsonb JSONB);'
+            create_table_query = f'CREATE TABLE IF NOT EXISTS {table_name} (vacancies_id SERIAL PRIMARY KEY, data_jsonb JSONB);'
 
             self.cur.execute(create_table_query)
-            self.conn.commit()  # Commit the table creation
+            self.conn.commit()
 
         except Exception as err:
             logger.error(f'Error creating table: {err}')
-            self.closeConnection()  # Close connection on error
-    def closeConnection(self):
+            self.close_connection()
+
+    def close_connection(self):
         if self.conn:
             self.cur.close()
             self.conn.close()
             logger.info('Database connection closed.')
 
-    def executeQuery(self, tableName: str, vacancyId: int, vacancyBody: dict):
-        sql = f'INSERT INTO {tableName} (vacancies_id, data_jsonb) VALUES (%s, %s)'
+    def execute_query(self, table_name: str, vacancy_id: str, vacancy_body: Dict[str, Any]) -> None:
+        sql = f'INSERT INTO {table_name} (vacancies_id, data_jsonb) VALUES (%s, %s)'
         try:
-            json_body = json.dumps(vacancyBody)  # Convert dictionary to JSON string
-            self.cur.execute(sql, (vacancyId, json_body))
+            json_body = json.dumps(vacancy_body)  # Convert dictionary to JSON string
+            self.cur.execute(sql, (vacancy_id, json_body))
             self.conn.commit()
-            logger.info('Data inserted successfully.')
         except IntegrityError:
             self.conn.rollback()
             logger.warning('Integrity error occurred. Rolling back transaction.')
         except Exception as err:
             logger.error(f'Error executing query: {err}')
-            self.conn.rollback()  # Rollback transaction on error
+            self.conn.rollback()
+
 
 if __name__ == '__main__':
     db = DataBase(dbname='mydb', username='postgres', host='localhost', password='2280')
-    db.connect(tableName='vacancies')
-    db.executeQuery(tableName='vacancies', vacancyId=123, vacancyBody={'body': '123', 'vacancies_id': 123})
-    db.closeConnection()
+    db.connect(table_name='vacancies')
+    db.execute_query(table_name='vacancies', vacancy_id=str(123), vacancy_body={'body': '123', 'vacancies_id': 123})
+    db.close_connection()
--- analysisIT/app/vacancyParser.py
+++ analysisIT/app/vacancyParser.py
@@ -18,142 +18,156 @@ DEFAULT_MAX_REC_RETURNED = 2000
 URL = 'https://api.hh.ru/vacancies/'
 
 
-class VacancyParser:
+class Worker:
     """
-    Осуществляет запрос к API Headhunter для получения списка идентификаторов (ids) вакансий в
-    каждом отдельном интервале и получает подробную информацию о каждой вакансии
+    Сlass with functions for splitting the time interval,getting vacancy
+    IDs and getting detailed information about each vacancy
     """
 
     def __init__(
             self,
-            dateFrom: date,
-            dateTo: date
+            date_from: date,
+            date_to: date
     ):
-        self.dateFrom = dateFrom
-        self.dateTo = dateTo
-        self.vacancyIdsSet = set()
-        self.vacancyQueue = queue.Queue()
-        self.timeIntervalsList = []
+        self.date_from = date_from
+        self.date_to = date_to
+        self.vacancy_ids_set = set()
+        self.vacancy_queue = queue.Queue()
+        self.time_intervals_list = []
         self.api = ApiClient(URL, ID_ROLES_LIST)
         self.db = DataBase(dbname='mydb', username='postgres', host='localhost', password='2280')
 
-    def run(self):
-        logger.info(f'Временной интервал: От {self.dateFrom} --> до --> {self.dateTo}')
-
-        while self.dateTo != self.dateFrom:
-            nextDate = self.processTimeInterval(self.dateTo, self.dateTo + timedelta(seconds=DEFAULT_MAX_STEP_SIZE))
-            self.dateTo = nextDate
-        print(self.timeIntervalsList)
-        self.getVacancyIds(self.timeIntervalsList)
-        print(self.vacancyIdsSet)
-        self.getVacancyBody(self.vacancyIdsSet)
-        self.addVacancyToDB(self.vacancyQueue)
-        logger.info('Процесс закончил свою работу')
-
-    def processTimeInterval(self, dateLeft, dateRight):
-        if dateRight > self.dateFrom:
-            dateRight = self.dateFrom
+    def process_time_interval(self, date_left: date, date_right: date) -> date:
+        """
+        Function checks the interval for the limit (2000) and, if necessary,
+        splits it into smaller intervals
+        :return: next date on which the step should be made
+        """
+        if date_right > self.date_from:
+            date_right = self.date_from
         params = {
             'per_page': 100,
             'page': 0,
             'professional_role': ID_ROLES_LIST,
-            'date_from': f'{dateLeft.isoformat()}',
-            'date_to': f'{dateRight.isoformat()}'}
+            'date_from': f'{date_left.isoformat()}',
+            'date_to': f'{date_right.isoformat()}'}
+        data = self.api.make_request(params=params)
 
-        data = self.api.makeRequest(params=params)
         if data == None:
-            return dateRight
+            return date_right
+        print(data['found'])
         if data['found'] <= DEFAULT_MAX_REC_RETURNED:
-            self.timeIntervalsList.append([dateLeft, dateRight])
-
+            self.time_intervals_list.append([date_left, date_right])
         else:
-            while dateRight != dateLeft:
-                self.timeIntervalsList.append([dateLeft, dateLeft + DEFAULT_MIN_STEP_SIZE])
-                dateLeft += DEFAULT_MIN_STEP_SIZE
-        return dateRight
+            while date_right != date_left:
+                self.time_intervals_list.append([date_left, date_left + DEFAULT_MIN_STEP_SIZE])
+                date_left += DEFAULT_MIN_STEP_SIZE
+        return date_right
 
-    def getVacancyIds(self, timeIntervalsList: List):
+    def get_vacancy_ids(self, time_intervals_list: List[List[date]]) -> None:
         """
-        Функция, проходящая по каждому интервалу времени из списка и добавляющая все идентификаторы вакансий в множество vacancyIdsSet.
-
-        :param timeIntervalsList: список интервалов времени на которые был разбит изначальный интервал
-        :retur
+        A function that goes through each time interval from the list and adds all
+        vacancy IDs to the vacancy_ids_set set.
+        :param time_intervals_list: list of time intervals into which the original interval was divided
         """
-        for timeInterval in timeIntervalsList:
-
+        for time_interval in time_intervals_list:
             for page in range(20):
                 params = {
                     'per_page': 100,
                     'page': page,
                     'professional_role': ID_ROLES_LIST,
-                    'date_from': f'{timeInterval[0].isoformat()}',
-                    'date_to': f'{timeInterval[1].isoformat()}'}
+                    'date_from': f'{time_interval[0].isoformat()}',
+                    'date_to': f'{time_interval[1].isoformat()}'}
 
-                data = self.api.makeRequest(params=params)
+                data = self.api.make_request(params=params)
                 if data == None:
                     break
-                idsSet = self.parseJsonIds(data)
-                self.vacancyIdsSet.update(idsSet)
+                ids_set = self.parse_json_ids(data)
+                self.vacancy_ids_set.update(ids_set)
                 if (data['pages'] - page) <= 1:
                     break
 
     @staticmethod
-    def parseJsonIds(vacanciesJson: Dict) -> Set[int]:
+    def parse_json_ids(vacancies_dict: Dict[str, Any]) -> Set[str]:
         """
-        Функция, распарсивающая JSON и выдающая множество ids
-        :param vacanciesJson: список интервалов времени на которые был разбит изначальный интервал
-        :retur: множество ids вакансий в данном json
+        Selects all ids from the dict
+        :return: set of vacancy ids in this dict
         """
-        idsSet = set()
-        for vacancy in vacanciesJson['items']:
+        ids_set = set()
+        for vacancy in vacancies_dict['items']:
             id = vacancy['id']
-            idsSet.add(id)
-        return idsSet
+            ids_set.add(id)
+        return ids_set
 
-    def getVacancyBody(self, vacancyIdsSet):
-        for id in vacancyIdsSet:
-
-            vacancyBody = self.api.makeRequest(vacancyId=int(id))
-
-            if vacancyBody == None:
+    def get_vacancy_body(self, vacancy_ids_set: Set[str]) -> None:
+        """
+        Gets the vacancy body by id
+        """
+        for id in vacancy_ids_set:
+            vacancy_body = self.api.make_request(vacancy_id=id)
+            if vacancy_body == None:
                 continue
-            self.vacancyQueue.put(vacancyBody)
-            if self.vacancyQueue.qsize() == 500:
-                self.addVacancyToDB(self.vacancyQueue)
+            self.vacancy_queue.put(vacancy_body)
+            if self.vacancy_queue.qsize() == 500:
+                self.add_vacancy_to_db(self.vacancy_queue)
 
-    def addVacancyToDB(self, vacancyQueue):
-        self.db.connect(tableName='vacancies')
-        while not vacancyQueue.empty():
-            vacancyBody = vacancyQueue.get()
-            vacancyId = vacancyBody['id']
+    def add_vacancy_to_db(self, vacancy_queue) -> None:
+        """
+        Adds vacancy bodies to the database
+        """
+        self.db.connect(table_name='vacancies')
+        while not vacancy_queue.empty():
+            vacancy_body = vacancy_queue.get()
+            vacancy_id = vacancy_body['id']
 
-            self.db.executeQuery(tableName='vacancies', vacancyId=vacancyId, vacancyBody=vacancyBody)
-        self.db.closeConnection()
+            self.db.execute_query(table_name='vacancies', vacancy_id=vacancy_id, vacancy_body=vacancy_body)
+        self.db.close_connection()
 
+    def run(self) -> None:
+        """
+        Function to start the parser
+        """
+        logger.info(f'Time interval: From {self.date_from} to {self.date_to}')
+
+        while self.date_to != self.date_from:
+            next_date = self.process_time_interval(self.date_to,
+                                                   self.date_to + timedelta(seconds=DEFAULT_MAX_STEP_SIZE))
+            self.date_to = next_date
+
+        logger.info(f'The selection of intervals is finished')
+
+        self.get_vacancy_ids(self.time_intervals_list)
+        self.get_vacancy_body(self.vacancy_ids_set)
+        self.add_vacancy_to_db(self.vacancy_queue)
+        logger.info('Процесс закончил свою работу')
 
 class ApiClient:
+    """
+    A class with functions for sending api requests
+    """
 
     def __init__(self,
                  base_url: str,
-                 idRoles: List[int] = None,
-                 countErrors: int = 0,
-                 maxCountErrors: int = 5,
-                 count: int = 0):
+                 id_roles: List[int] = None,
+                 count_errors: int = 0,
+                 max_count_errors: int = 5,
+                 requests_send_count: int = 0):
+
         self.base_url = base_url
-        self.idRoles = idRoles
-        self.countErrors = countErrors
-        self.maxCountErrors = maxCountErrors
-        self.count = count
+        self.id_roles = id_roles
+        self.count_errors = count_errors
+        self.max_count_errors = max_count_errors
+        self.requests_send_count = requests_send_count
 
-    def makeRequest(self,
-                    vacancyId: int = None,
-                    params=None,
-                    proxy: str = None,
-                    timeout: int = 5,
-                    retry: int = 2):
+    def make_request(self,
+                     vacancy_id: str = None,
+                     params: Dict[str, Any] = None,
+                     proxy: str = None,
+                     timeout: int = 5,
+                     retry: int = 2) -> Union[Dict[str, Any], str, None]:
 
-        if vacancyId:
-            url = self.base_url + f'{vacancyId}'
+        if vacancy_id:
+            url = self.base_url + f'{vacancy_id}'
         else:
             url = self.base_url
         try:
@@ -161,9 +175,9 @@ class ApiClient:
             req.raise_for_status()
 
         except requests.exceptions.ConnectionError as e:
-            self.countErrors += 1
-            if self.countErrors > self.maxCountErrors:
-                self.countErrors = 0
+            self.count_errors += 1
+            if self.count_errors > self.max_count_errors:
+                self.count_errors = 0
                 return None
 
             logger.info(f'{e} retry {retry} proxy {proxy}')
@@ -172,46 +186,55 @@ class ApiClient:
             return 'change the proxy'
 
         except Exception as err:
-            self.countErrors += 1
-            if self.countErrors > self.maxCountErrors:
-                self.countErrors = 0
+            self.count_errors += 1
+            if self.count_errors > self.max_count_errors:
+                self.count_errors = 0
                 return None
 
             time.sleep(4)
             if retry:
                 logger.info(f'{err}. retry {retry} proxy {proxy}')
-                return self.makeRequest(vacancyId, params, proxy, retry=(retry - 1))
+                return self.make_request(vacancy_id, params, proxy, retry=(retry - 1))
             else:
                 return 'change the proxy'
 
         else:
-            self.countErrors = 0
-            self.count += 1
+            self.count_errors = 0
+            self.requests_send_count += 1
             data = req.content.decode()
             data = json.loads(data)
-
             if 'items' in data:
                 if data['items'] == [] and retry:
-                    data = self.makeRequest(vacancyId, params, proxy, retry=(retry - 1))
+                    data = self.make_request(vacancy_id, params, proxy, retry=(retry - 1))
                 elif data['items'] == []:
                     return None
             return data
         finally:
             if req != None:
                 req.close()
-                if self.count >= 10:
+                if self.requests_send_count >= 10:
                     time.sleep(1)
-                    self.count = 0
+                    self.requests_send_count = 0
 
 
 def get_date(left, timedelta_in_seconds):
+    """
+    Used mostly for testing
+    """
     now = datetime.now() - timedelta(seconds=left)
     date_to = (now - timedelta(minutes=now.minute % 5)).replace(second=0, microsecond=0)
     list_time = [date_to, date_to - timedelta(seconds=timedelta_in_seconds)]
     return list_time
 
 
-if __name__ == '__main__':
-    li = get_date(0, 5000)
-    pars = VacancyParser(*li)
+def main() -> None:
+    """
+    Used mostly for testing; this module is not usually run standalone
+    """
+    li = get_date(0, 300)
+    pars = Worker(*li)
     pars.run()
+
+
+if __name__ == '__main__':
+    main()
